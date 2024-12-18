ac3f8594b4b0562d60cfe7cff04aa850419b4fa4
batrivieredev <baptiste.rivierefr@gmail.com>
2024-12-18 11:12:43 +0100

    create many files and update readme

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#endif
