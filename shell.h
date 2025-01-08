48ab47e8e8480b27873dc1a0f86163d265e59e9d
batrivieredev <baptiste.rivierefr@gmail.com>
2025-01-08 13:58:19 +0100

    shell.h

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+
+/* Function Prototypes */
+void prompt(void);
+void execute_command(char *command);
+char *read_input(void);
+void handle_error(char *command);
+
+#endif
