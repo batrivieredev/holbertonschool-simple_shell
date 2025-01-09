6bd2a8bc29422df9bbe00b8b1558221d04a9fb88
Braga <ibrahim.houmaidi@gmail.com>
2025-01-09 15:18:00 +0100

    update exit

--- exit_shell.c
+++ exit_shell.c
@@ -1,13 +1,24 @@
 #include "shell.h"
 
+/**
+ * exit_shell - Fonction pour quitter le shell
+ * @args: Tableau d'arguments
+ * @last_status: Statut de la dernière commande exécutée
+ *
+ * Description: Cette fonction permet de quitter le shell avec un statut
+ * spécifié par l'utilisateur ou par défaut avec le statut de la dernière
+ * commande exécutée. Si le statut fourni est invalide (négatif), un message
+ * d'erreur est affiché et le shell quitte avec le statut de la dernière
+ * commande.
+ */
 void exit_shell(char **args, int last_status)
 {
-	int status = last_status; /* Default to the last command's status */
+	int status = last_status; /*utiliser le statut de la dernière commande */
 
-	if (args[1]) /* Check for an optional exit status */
+	if (args[1]) /* Vérifier s'il y a un statut de sortie optionnel */
 	{
 		status = atoi(args[1]);
-		if (status < 0) /* Invalid exit status */
+		if (status < 0) /* Statut de sortie invalide */
 		{
 			write(STDERR_FILENO, "exit: Invalid status\n", 21);
 			status = last_status;
