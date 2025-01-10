23fcdf059ad8add0f7e3da8fabf5625ff3a35908
batrivieredev <baptiste.rivierefr@gmail.com>
2025-01-10 10:17:31 +0100

    maj

--- handle_env.c
+++ handle_env.c
@@ -1,25 +1,12 @@
 #include "shell.h"
 
-
-/**
- *Déclaration de la variable externe
- *environ qui contient les variables d'environnement
- *
- */
-
-extern char **environ;
-
 /**
  * handle_env - Affiche les variables d'environnement
  */
 void handle_env(void)
 {
-	/*Déclaration d'un pointeur vers un tableau de chaînes de caractères*/
 	char **env;
 
-	/*Boucle à travers chaque variable d'environnement et l'affiche*/
-	{
-		/*Affiche la variable d'environnement actuelle*/
+	for (env = environ; *env != NULL; env++)
 		printf("%s\n", *env);
-	}
 }
--- parse_exit_status.c
+++ parse_exit_status.c
@@ -2,30 +2,30 @@
 #include "shell.h"
 
 /**
- * parse_exit_status - Analyse le statut de sortie de la commande
- * @cmd: La chaîne de commande
+ * parse_exit_status - Parses the exit status from the command
+ * @cmd: The command string
  *
- * Return: Le statut de sortie (par défaut est 0)
+ * Return: The exit status (default is 0)
  */
 int parse_exit_status(char *cmd)
 {
-	char *arg = cmd + 4; /* Déplace le pointeur après "exit" */
+	char *arg = cmd + 4; /* Move pointer past "exit" */
 	int status = 0;
 	int i;
 
-	while (*arg == ' ') /* Ignore les espaces après "exit" */
+	while (*arg == ' ') /* Skip any spaces after "exit" */
 		arg++;
 
-	if (*arg != '\0') /* S'il y a quelque chose après "exit" */
+	if (*arg != '\0') /* If there is something after "exit" */
 	{
 		for (i = 0; arg[i]; i++)
 		{
-			if (!isdigit(arg[i])) /* Vérifie si l'argument n'est pas un chiffre */
+			if (!isdigit(arg[i])) /* Check if the argument is not a digit */
 			{
-				return (0); /* Retourne 0 pour continuer à faire tourner le shell */
+				return (0); /* Return 0 to keep the shell running */
 			}
 		}
-		status = atoi(arg); /* Convertit l'argument en entier */
+		status = atoi(arg); /* Convert the argument to an integer */
 	}
 
 	return (status);
